174cc9fe1d43d6a11eda77e284933f5eb7cda048
XABOK <ivannikovilya97@mail.ru>
2023-02-17 14:45:40 +0700

    Фикстура

--- test_reqres_GUI/conftest.py
+++ test_reqres_GUI/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.maximize_window()
     browser.implicitly_wait(10)
     browser.get(url="https://reqres.in/")
