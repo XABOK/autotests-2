ff4ae34209523e3fe9987b5b65c1f70c56d209c8
XABOK <ivannikovilya97@mail.ru>
2023-03-28 10:12:47 +0700

    базовые методы

--- reqres_GUI/pages/base_page.py
+++ reqres_GUI/pages/base_page.py
@@ -5,17 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     """Общие методы"""
 
-    def __init__(self, browser, url):
-        self.browser = browser
+    def __init__(self, driver, url):
+        self.driver = driver
         self.url = url
 
     def open(self):
-        self.browser.get(self.url)
+        self.driver.get(self.url)
 
     def element_is_clickable(self, locator, timeout=10):
         """Является ли элемент кликабельным"""
-        return WebDriverWait(self.browser, timeout).until(EC.element_to_be_clickable(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
     def element_are_visible(self, locator, timeout=10):
         """Является ли элемент видимым"""
-        return WebDriverWait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
